85e303d43f58506b62b6380165141fee336ce6af
root <root@beaglebone.localdomain>
2017-01-19 22:09:31 +0000

    finished initial version of c program

--- ECE_Capstopne/reader.c
+++ ECE_Capstopne/reader.c
@@ -1,10 +1,17 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(){
   static const char* ADC_file= "/sys/bus/iio/devices/iio:device0/in_voltage0_raw";
+  char buf[BUF_MAX];
+  char val[4];
   while(1){
     //read ADC file and spit contents to STDOUT for it to be piped to the network layer
-
+    int bytes_read = snprintf(buf, sizeof(buf),ADC_file);
+    fd = open(buf,O_RDONLY);
+    //do error checking
+    read(fd,&val,4);
+    printf("%f\n",val);
   }
   return 0;
 }
