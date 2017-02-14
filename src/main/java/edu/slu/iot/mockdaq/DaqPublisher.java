efbc5f6bc49e938004eb6c3235984d2f68f7518a
Demo User <debian@beaglebone.localdomain>
2017-02-14 22:15:36 +0000

    added ADC code under src/main/c and added realdaq package with DaqPublisher

---
+++ src/main/java/edu/slu/iot/mockdaq/DaqPublisher.java
@@ -0,0 +1,60 @@
+package edu.slu.iot.mockdaq;
+
+import com.amazonaws.services.iot.client.AWSIotMessage;
+import com.amazonaws.services.iot.client.AWSIotQos;
+
+import edu.slu.iot.Publisher;
+
+public class DaqPublisher extends Publisher {
+	
+	private String sessionID;
+	
+	public DaqPublisher(String topic, AWSIotQos qos, String sessionID) {
+		super(topic, qos);
+		this.sessionID = sessionID;
+	}
+
+	@Override
+    public void run() {
+    	
+        while (true) {
+        	
+        	long millis = System.currentTimeMillis();
+            String payload = "{\"time\": " + millis + ",\"session\": " + "\"" + sessionID + "\"" + ",\"value\": " + Math.sin((double) millis / 1000) + "}";
+            
+            AWSIotMessage message = new NonBlockingPublishListener(topic, qos, payload);
+            
+            publish(message);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(System.currentTimeMillis() + ": NonBlockingPublisher was interrupted");
+                return;
+            }
+        }
+    }
+	
+	private class NonBlockingPublishListener extends AWSIotMessage {
+
+	    public NonBlockingPublishListener(String topic, AWSIotQos qos, String payload) {
+	        super(topic, qos, payload);
+	    }
+
+	    @Override
+	    public void onSuccess() {
+	        System.out.println(System.currentTimeMillis() + ": >>> " + getStringPayload());
+	    }
+
+	    @Override
+	    public void onFailure() {
+	        System.out.println(System.currentTimeMillis() + ": publish failed for " + getStringPayload());
+	    }
+
+	    @Override
+	    public void onTimeout() {
+	        System.out.println(System.currentTimeMillis() + ": publish timeout for " + getStringPayload());
+	    }
+
+	}
+}
---
+++ test.sh
@@ -0,0 +1,3 @@
+./src/main/c/ECE_Capstone_ADC/py_read.py
+./src/main/c/ECE_Capstone_ADC/reader &
+java -cp target/mock-daq-0.0.jar edu.slu.iot.realdaq.PublishSample
