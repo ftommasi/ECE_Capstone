7b3b9d2414f387fb309105de23d448bf68c45f59
Christopher Lucas <cwlucas41@orodruin.local>
2017-02-16 16:09:58 -0600

    added sample class with sort test

---
+++ src/main/java/edu/slu/iot/realdaq/Sample.java
@@ -0,0 +1,36 @@
+package edu.slu.iot.realdaq;
+
+public class Sample implements Comparable<Sample> {
+	
+	private String deviceID;
+	private String sessionID;
+	private long timestamp;
+	private float value;
+	
+	public Sample(String deviceID, String sessionID, long timestamp, float value) {
+		this.deviceID = deviceID;
+		this.sessionID = sessionID;
+		this.timestamp = timestamp;
+		this.value = value;
+	}
+	
+	public String getDeviceID() {
+		return deviceID;
+	}
+	public String getSessionID() {
+		return sessionID;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public float getValue() {
+		return value;
+	}
+
+	@Override
+	public int compareTo(Sample o) {
+		// sorts by increasing timestamp
+		return ((Long) timestamp).compareTo(o.timestamp);
+	}
+
+}
