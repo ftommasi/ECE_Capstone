5c6bfb4ecd3ab82048abb087a1099fc51c9e5187
Demo User <mmasito123@yahoo.com>
2017-04-11 21:39:07 +0000

    a lot of ADC changes :hankey:

--- py_read.py
+++ py_read.py
@@ -4,11 +4,10 @@ import Adafruit_BBIO.ADC as ADC
 import time 
 
 ADC.setup()
-while True:
-  start =time.clock()
-  ADC.read_raw("P9_39")
-  end = time.clock()
-  #print "end - start = ", end-start
+start =time.clock()
+ADC.read_raw("P9_39")
+end = time.clock()
+#print "end - start = ", end-start
 
-  print "raw: ",   ADC.read_raw("P9_39") , "val: ", ADC.read("P9_39")*1.8
+print "raw: ",   ADC.read_raw("P9_39") , "val: ", ADC.read("P9_39")*1.8
 
--- reader.c
+++ reader.c
@@ -1,50 +1,109 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
+#include <fcntl.h>
 
-int convert_raw_ADC_to_volts(int raw_adc){
+#define RESOLUTION 10000000
 
-  return (raw_adc*1.8)/4096;
+void calculate_sleep_time(float frequency, struct timespec* time, struct timespec* time_elapsed){
+
+  float in_seconds = 1.0f/frequency;
+  float partial = in_seconds - (int) in_seconds;
+  /*
+  time->tv_sec = 
+    ((in_seconds - partial) - time_elapsed->tv_sec < 0 ? 
+     0  : 
+     (in_seconds - partial) - time_elapsed->tv_sec);
+
+  time->tv_nsec =
+    ((long)partial*RESOLUTION - time_elapsed->tv_nsec < 0 ?
+     0 :
+     (long)partial*RESOLUTION - time_elapsed->tv_nsec );
+  */
+  
+  time->tv_sec = 
+    (time_elapsed->tv_sec - (in_seconds - partial) < 0 ? 
+     0  : 
+     (in_seconds - partial) - time_elapsed->tv_sec);
+
+  time->tv_nsec =
+    (time_elapsed->tv_nsec - (long)partial*RESOLUTION  < 0 ?
+     0 :
+     (long)partial*RESOLUTION - time_elapsed->tv_nsec );
+
+
 
 }
 
+int main(int argc, char** argv){
+  int pid = fork();
+  if(pid ==-1){
+    printf("error forking\n");
+  }
+  if(pid == 0){
+    execve("py_read.py",NULL,NULL);
+    _exit(-1); //execve never returns
+  }
+  else{
+    int status;
+    wait(pid,&status,0);
+    static const char* ADC_file= "/sys/bus/iio/devices/iio\:device0/in_voltage0_raw";
+    char buf[5];
+    FILE* sample_file;
+    struct timespec walltime;
+    struct timespec prevwalltime;
+    struct timespec time_elapsed;
+    struct timespec sleep_time;
 
-int main(){
-  static const char* ADC_file= "/sys/bus/iio/devices/iio\:device0/in_voltage0_raw";
-  char buf[1024];
-  clock_t start,end;
-  FILE* sample_file;
-  sample_file = fopen("SAMPLE-SESSION","a+");
-  int count = 0;
-  while(count < 100000){
-    start = clock();
-    snprintf(buf,sizeof(buf),ADC_file);
-    end = clock();
-    int fd = open(buf,0);
-    int buflen;
-    int sample_count = 0;
-    count++;
-    while((buflen=read(fd,buf,100))>0){
-      //write(1,buf,buflen);
-      char tempbuf[5];
-      memcpy(tempbuf,&buf[0],4);
-      tempbuf[4] ='\0';
-      //printf("buf[42]: %s\n",tempbuf);
-      fprintf(sample_file,"%s\n",tempbuf);
-      //fprintf(sample_file,"%d\n",count);
-      //write(fileno(sample_file),count_buf,sizeof(count_buf));
-      int in_volts = atoi(buf);
-     // snprintf();
-     // fprintf(sample_file,buf);
-      if(sample_count % 100 == 0){
-        //TODO: implement functionality to create a new file every 100 samples
+    char flag = 0;
+    sample_file = fopen("DAQ-SESSION","a+");
+    clockid_t clock_id;
+    
+    if(clock_getcpuclockid(getpid(),&clock_id) == -1){
+      printf("error setting up clock!\n");
+      exit(0);
+    }
+
+    prevwalltime.tv_sec = 0;
+    prevwalltime.tv_nsec =0;
+    int ADC_fd;
+    int readval = -1;
+    ADC_fd=open(ADC_file, O_RDONLY | O_NONBLOCK);
+    FILE* ADC;     //fcntl(ADC_fd, F_SETFL, fcntl(ADC_fd,F_GETFL),0);
+    //ADC = fopen(ADC_file,"r+");
+    while(1){ 
+      /*
+      char* map = mmap(NULL, sizeof(char)*53, PROT_READ, MAP_SHARED, fd,0);  
+      read(ADC_fd,buf,5);
+      for (int i = 1; i <=sizeof(char)*53; ++i) {
+        printf("%d: %d\n", i, map[i]);
+        }
+        */
+      //snprintf(buf,100,ADC_file);
+      read(ADC_fd, &buf, 5); 
+      prevwalltime.tv_sec = walltime.tv_sec;
+      prevwalltime.tv_nsec = walltime.tv_nsec;
+      if(clock_gettime(clock_id,&walltime)==0){
+        //known constants for output string of ADC. no need for strlen or memcpy when doing this. speed
+        //fprintf(sample_file,"%s %lu\n",buf ,walltime.tv_nsec);
+        time_elapsed.tv_sec =  walltime.tv_sec - prevwalltime.tv_sec;
+        time_elapsed.tv_nsec =  walltime.tv_nsec - prevwalltime.tv_nsec;
+        printf("%s %lu\n",buf,time_elapsed.tv_nsec);
+        //printf("%s %d\n",buf,buf);
+      }
+      else{
+        printf("gettime error!!!\n");
       }
 
-      sample_count++;
-      //printf("%f V\n");
+      calculate_sleep_time((float)1/atof(argv[1]), &sleep_time,&time_elapsed);
+      //nanosleep(&sleep_time,NULL);
+
+   
     }
-  //  printf("\n---TIME: %f---\n",(double)(end-start)/CLOCKS_PER_SEC);
+
+    close(ADC_fd);
+    fclose(sample_file);
+    return 0;
   }
-  fclose(sample_file);
-  return 0;
 }
