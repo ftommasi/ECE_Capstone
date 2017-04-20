5d13a719a042558c39a9ce7cfe20d5407481c57c
Demo User <mmasito123@yahoo.com>
2017-04-20 22:03:45 +0000

    variable frequency done

--- reader.c
+++ reader.c
@@ -1,63 +1,109 @@
+#define _GNU_SOURCE 1
+#include "stdio.h"
+#include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/time.h>
+//not very portable ... oops
+#include "libpruio-0.2/src/c_wrapper/pruio.h"
+#include "libpruio-0.2/src/c_wrapper/pruio_pins.h"
 #include <time.h>
-#include <pthread.h>
-#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
 
-#define RESOLUTION 10000000
+//"/home/debian/ECE-Capstone-Demo/src/main/c/ECE_Capstone/libpruio-0.2/src/c_examples"
+
+//! The header pin to use.
+#define PIN P8_07
+
+#define bool char
+#define RESOLUTION 1000000000
+
+static pruIo *io; 
 
 void calculate_sleep_time(float frequency, struct timespec* time, struct timespec* time_elapsed){
 
   float in_seconds = 1.0f/frequency;
   float partial = in_seconds - (int) in_seconds;
-  /*
-  time->tv_sec = 
-    ((in_seconds - partial) - time_elapsed->tv_sec < 0 ? 
-     0  : 
-     (in_seconds - partial) - time_elapsed->tv_sec);
-
-  time->tv_nsec =
-    ((long)partial*RESOLUTION - time_elapsed->tv_nsec < 0 ?
-     0 :
-     (long)partial*RESOLUTION - time_elapsed->tv_nsec );
-  */
   
   time->tv_sec = 
-    (time_elapsed->tv_sec - (in_seconds - partial) < 0 ? 
-     0  : 
-     (in_seconds - partial) - time_elapsed->tv_sec);
+         (long)(in_seconds - partial);
 
   time->tv_nsec =
-    (time_elapsed->tv_nsec - (long)partial*RESOLUTION  < 0 ?
-     0 :
-     (long)partial*RESOLUTION - time_elapsed->tv_nsec );
+    
+     (long)(partial*RESOLUTION);
 
 
 
 }
 
-int main(int argc, char** argv){
-  int pid = fork();
-  if(pid ==-1){
-    printf("error forking\n");
-  }
-  if(pid == 0){
-    execve("py_read.py",NULL,NULL);
-    _exit(-1); //execve never returns
+
+
+
+//function to wait for keystroke
+int
+isleep(unsigned int mseconds)
+{
+  fd_set set;
+  struct timespec timeout;
+
+  /* Initialize the file descriptor set. */
+  FD_ZERO(&set);
+  FD_SET(STDIN_FILENO, &set);
+
+  /* Initialize the timeout data structure. */
+  timeout.tv_sec = 0;
+  timeout.tv_nsec = mseconds ;
+
+  return TEMP_FAILURE_RETRY(select(FD_SETSIZE,
+                                   &set, NULL, NULL,
+                                   &timeout));
+}
+
+//handle cleanup if things go south
+int sighandler(int signum){
+  pruio_destroy(io);       /* destroy driver structure */
+  exit(-1);
+}
+
+//! The main function.
+int main(int argc, char **argv)
+{
+  signal(SIGINT,sighandler);
+  signal(SIGSEGV,sighandler);
+  
+  struct timespec walltime;
+  struct timespec prevwalltime;
+  struct timespec time_elapsed;
+  struct timespec sleep_time;
+  struct timespec delay_time;
+  struct timespec delay_start;
+  float frequency;
+  if(argc == 1){
+    frequency = 50000.0f;
   }
   else{
-    int status;
-    wait(pid,&status,0);
-    static const char* ADC_file= "/sys/bus/iio/devices/iio\:device0/in_voltage0_raw";
-    char buf[5];
-    FILE* sample_file;
-    struct timespec walltime;
-    struct timespec prevwalltime;
-    struct timespec time_elapsed;
-    struct timespec sleep_time;
+    frequency = atof(argv[1]);
+  }
 
-    char flag = 0;
-    sample_file = fopen("DAQ-SESSION","a+");
+  long i = 0;
+  int rc;
+  io = pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1); //! create new driver structure
+  do {
+    if (io->Errr) {
+               printf("initialisation failed (%s)\n", io->Errr); break;}
+
+    if (pruio_config(io, 1, 0x1FE, 0, 4)) {
+                       printf("config failed (%s)\n", io->Errr); break;}
+
+    struct termios oldt, newt; //             make terminal non-blocking
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    newt.c_cc[VMIN] = 0;
+    newt.c_cc[VTIME] = 0;
+    
     clockid_t clock_id;
     
     if(clock_getcpuclockid(getpid(),&clock_id) == -1){
@@ -67,43 +113,34 @@ int main(int argc, char** argv){
 
     prevwalltime.tv_sec = 0;
     prevwalltime.tv_nsec =0;
-    int ADC_fd;
-    int readval = -1;
-    ADC_fd=open(ADC_file, O_RDONLY | O_NONBLOCK);
-    FILE* ADC;     //fcntl(ADC_fd, F_SETFL, fcntl(ADC_fd,F_GETFL),0);
-    //ADC = fopen(ADC_file,"r+");
-    while(1){ 
-      /*
-      char* map = mmap(NULL, sizeof(char)*53, PROT_READ, MAP_SHARED, fd,0);  
-      read(ADC_fd,buf,5);
-      for (int i = 1; i <=sizeof(char)*53; ++i) {
-        printf("%d: %d\n", i, map[i]);
-        }
-        */
-      //snprintf(buf,100,ADC_file);
-      read(ADC_fd, &buf, 5); 
-      prevwalltime.tv_sec = walltime.tv_sec;
-      prevwalltime.tv_nsec = walltime.tv_nsec;
-      if(clock_gettime(clock_id,&walltime)==0){
-        //known constants for output string of ADC. no need for strlen or memcpy when doing this. speed
-        //fprintf(sample_file,"%s %lu\n",buf ,walltime.tv_nsec);
-        time_elapsed.tv_sec =  walltime.tv_sec - prevwalltime.tv_sec;
-        time_elapsed.tv_nsec =  walltime.tv_nsec - prevwalltime.tv_nsec;
-        printf("%s %lu\n",buf,time_elapsed.tv_nsec);
-        //printf("%s %d\n",buf,buf);
-      }
-      else{
-        printf("gettime error!!!\n");
-      }
 
-      calculate_sleep_time((float)1/atof(argv[1]), &sleep_time,&time_elapsed);
-      //nanosleep(&sleep_time,NULL);
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-   
+    while(1) { //                      run loop until keystroke
+      clock_gettime(clock_id,&prevwalltime);
+       printf("%lu:%lu %d\n", 
+          time_elapsed.tv_sec,
+          time_elapsed.tv_nsec,
+          io->Adc->Value[1] 
+          );
+      //known constants for output string of ADC. no need for strlen or memcpy when doing this. speed
+      //calculate_sleep_time((float)1/atof(argv[1]), &sleep_time,&time_elapsed);
+      
+      calculate_sleep_time(frequency, &sleep_time,&time_elapsed);
+      //printf("[%f]sleeping for %lu:%lu\n",frequency,sleep_time.tv_sec,sleep_time.tv_nsec);
+      nanosleep(&sleep_time,NULL);
+      clock_gettime(clock_id,&walltime);
+      
+      time_elapsed.tv_sec =  walltime.tv_sec - prevwalltime.tv_sec;
+      time_elapsed.tv_nsec =  walltime.tv_nsec - prevwalltime.tv_nsec;
+
+      //fflush(STDIN_FILENO);
     }
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); //           reset terminal
 
-    close(ADC_fd);
-    fclose(sample_file);
-    return 0;
-  }
+    //printf("\n");
+  } while (0);
+  pruio_destroy(io);       /* destroy driver structure */
+	return 0;
 }
